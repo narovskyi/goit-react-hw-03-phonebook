30f5447fe9d904b21974b7694e05fff3188f797e
Oleksii <o.narovskyi@gmail.com>
2023-05-11 21:47:11 +0300

    homework done

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -7,13 +7,23 @@ import { Container, Title } from "components/Phonebook/Phonebook.styled"
 
 class Phonebook extends Component {
     state = {
-        contacts: [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'}
-        ],
+        contacts: [],
         filter: '',
+    }
+
+    componentDidMount() {
+        const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+        if (parsedContacts) {
+            this.setState({
+                contacts: [...parsedContacts]
+            });
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.contacts !== prevState.contacts) {
+            localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+        }
     }
 
     inputValueHandler = (e) => {
